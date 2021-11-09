678d3fef6ca96b8bc00b3a53d35674fcda205a2f
RUSLAN D <x.stelvia@gmail.com>
2021-11-09 12:18:53 +0600

    1st

--- bot.py
+++ bot.py
@@ -1,2 +1,22 @@
-import datetime
-import time
+from aiogram import Bot, Dispatcher, executor, types
+import config
+import logging
+
+# log level
+logging.basicConfig(level=logging.INFO)
+
+# bot init
+bot = Bot(token=config.TOKEN)
+dp = Dispatcher(bot)
+
+# echo
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer(message.text)
+
+
+# run long polling
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
