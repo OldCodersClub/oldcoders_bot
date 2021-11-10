0811d71d5faacf7f0f43489a929515920b3186af
RUSLAN D <x.stelvia@gmail.com>
2021-11-10 20:07:19 +0600

    fix

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
-s3 = os.environ.get("BOT_TOKEN")
-
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+BOT_OWNER = os.environ.get("BOT_OWNER")
 
 # group ids or account ids can be retrieved with @username_to_id_bot
-BOT_TOKEN = "2111338135:AAGhltOYCNaitXNwY0jh4z2A-3ba2-VgBVU"
-BOT_OWNER = "oldcoders_bot"
+# BOT_TOKEN = "2111338135:AAGhltOYCNaitXNwY0jh4z2A-3ba2-VgBVU"
+# BOT_OWNER = "oldcoders_bot"
 
 # s3 = S3Connection(os.environ['BOT_TOKEN'], os.environ['BOT_OWNER'])
--- handlers/personal_actions.py
+++ handlers/personal_actions.py
@@ -1,8 +1,7 @@
-import os
 from aiogram import types
 from dispatcher import dp
 import config
-from config import s3 as s3
+from config import BOT_OWNER as BOT_OWNER
 
 
 @dp.message_handler()
@@ -14,7 +13,7 @@ async def echo(message: types.Message):
         case "привет Деды":
             await message.answer("И тобе привет старина")
         case "/secret":
-            await message.answer(f'{s3}')
+            await message.answer(f'{BOT_OWNER}')
 
     # if message.text == "/инфо":
     #     await message.answer("https://github.com/OldCodersClub/faq")
