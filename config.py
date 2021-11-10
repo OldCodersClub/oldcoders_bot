2da82bc77146ee498711e80f8add03278ceac1f0
RUSLAN D <x.stelvia@gmail.com>
2021-11-10 19:35:52 +0600

    fix

--- config.py
+++ config.py
@@ -7,11 +7,11 @@
 
 import boto
 c = boto.connect_s3()
-BOT_TOKEN = c.get_bucket('BOT_TOKEN')
+# BOT_TOKEN = c.get_bucket('BOT_TOKEN')
 # BOT_OWNER = c.get_bucket('BOT_OWNER')
 
 # group ids or account ids can be retrieved with @username_to_id_bot
-# BOT_TOKEN = "2111338135:AAGhltOYCNaitXNwY0jh4z2A-3ba2-VgBVU"
+BOT_TOKEN = "2111338135:AAGhltOYCNaitXNwY0jh4z2A-3ba2-VgBVU"
 BOT_OWNER = "oldcoders_bot"
 
 # s3 = S3Connection(os.environ['S3_KEY'], os.environ['S3_SECRET'])
--- handlers/personal_actions.py
+++ handlers/personal_actions.py
@@ -11,6 +11,8 @@ async def echo(message: types.Message):
             await message.answer("https://github.com/OldCodersClub/faq")
         case "привет Деды":
             await message.answer("И тобе привет старина")
+        case "/secret":
+            await message.answer("{c}")
 
     # if message.text == "/инфо":
     #     await message.answer("https://github.com/OldCodersClub/faq")
