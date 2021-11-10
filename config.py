5c5a11776027cd603bba18d0a9f08e9afa86c678
RUSLAN D <x.stelvia@gmail.com>
2021-11-10 21:26:53 +0600

    fix

--- config.py
+++ config.py
@@ -5,9 +5,10 @@ import os
 # env.read_env()
 
 BOT_TOKEN2 = str(os.environ.get('BOT_TOKEN'))+'____'
+BOT_TOKEN = str(os.environ.get('BOT_TOKEN'))
 
 
-BOT_TOKEN = "2111338135:AAGhltOYCNaitXNwY0jh4z2A-3ba2-VgBVU"
+# BOT_TOKEN = "2111338135:AAGhltOYCNaitXNwY0jh4z2A-3ba2-VgBVU"
 BOT_OWNER = str(os.environ.get('BOT_OWNER'))
 
 # BOT_TOKEN2 = env.str("BOT_TOKEN")
