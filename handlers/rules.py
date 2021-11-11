3adc3beb2e749b0e1bacc76d1fb32e218e38af67
RUSLAN D <x.stelvia@gmail.com>
2021-11-11 09:20:40 +0600

    fix

--- handlers/rules.py
+++ handlers/rules.py
@@ -6,8 +6,11 @@ import config
 @dp.message_handler()
 async def echo(message: types.Message):
 
-    match message.text:
+    match message.text.lower():
         case "/инфо" | "/info":
             await message.answer("https://github.com/OldCodersClub/faq")
-        case "привет":
-            await message.answer("И тобе привет старина")
+
+    if "привет деды" in message.text.lower():
+        await message.answer("И тобе привет Cтарина")
+    elif "вступил(а) в группу" in message.text.lower():
+        await message.answer("Добро пожаловать\nСоветуем ознакомиться с дедовским архивом знаний\n\n\nhttps://github.com/OldCodersClub/faq")
