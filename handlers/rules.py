3598f5ac7d9535b7774b902cc4ce47ee2018b412
Xeno Sama <71543129+xeno-sama@users.noreply.github.com>
2022-07-09 21:34:58 +0600

    Update rules.py

--- handlers/rules.py
+++ handlers/rules.py
@@ -2,6 +2,7 @@ from aiogram import types
 from aiogram.dispatcher.webhook import SendMessage
 from dispatcher import dp
 import config
+from datetime import datetime
 
 
 @dp.message_handler()
@@ -16,7 +17,10 @@ async def echo(message: types.Message):
             await message.answer(message.chat.id)
         case "/инфа":
             await message.answer("[\n* Клуб дедов-программистов FAQ *\n\nВ этом репозитории находится полезная информация, собранная участниками чата. Дорожные карты(roadmaps), шпаргалки, ссылки на курсы и самые полезные статьи.\n\nЭлементарный уровень\n - Начало работы с Вебом\n - Язык программирования Python\n - Введение в программирование. Подборка материалов по остальным языкам.\n\nПродвинутый уровень\n - Все для веб-разработчика\n - Все для Python-разработчика\n - Пет-проекты разной сложности: ресурсы, идеи\n - Задачи для прокачки + тестовые задания\n\nМатериалы и ресурсы других ИТ-сообществ:\n - Что учить веб-разработчику. Разные гайды 2021\n - Карты развития Python 2021\n\nПрофессии\n - В тестировщики хочу, пусть меня научат!\n - Кто такие бизнес-аналитики и как стать БА?\n\nПолезное:\n - Git и GitHub\n - Как мы учим английский язык](https://github.com/OldCodersClub/faq)", parse_mode='markdown', disable_web_page_preview=True)
-
+        case "алиса сколько времени?":
+            await message.answer(datetime.now())
+            
+            
     if "привет деды" in message.text.lower():
         await message.answer("И тобе привет Cтарина")
     elif "вступил(а) в группу" in message.text.lower():
